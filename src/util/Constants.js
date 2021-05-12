294eca6be443885ca8327cf02c07849a1311d712
Brandon <email@zendrex.com>
2021-05-12 03:31:53 -0700

    added timespan type for arguments, etc
    this feature is untested currently, if it ends up being not needed, it will be removed.

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -23,6 +23,7 @@ module.exports = {
         DATE: 'date',
         COLOR: 'color',
         USER: 'user',
+        TIMESPAN: 'timespan',
         USERS: 'users',
         MEMBER: 'member',
         MEMBERS: 'members',
@@ -83,6 +84,38 @@ module.exports = {
         OWNER: 'owner',
         GUILD: 'guild',
         DM: 'dm'
+    },
+    TimeUnits: {
+        years: {
+            label: '(?:years?|y)',
+            value: 1000 * 60 * 60 * 24 * 365
+        },
+        months: {
+            label: '(?:months?|mo)',
+            value: 1000 * 60 * 60 * 24 * 30
+        },
+        weeks: {
+            label: '(?:weeks?|w)',
+            value: 1000 * 60 * 60 * 24 * 7
+        },
+        days: {
+            label: '(?:days?|d)',
+            value: 1000 * 60 * 60 * 24
+        },
+        hours: {
+            label: '(?:hours?|hrs?|h)',
+            value: 1000 * 60 * 60
+        },
+        minutes: {
+            label: '(?:minutes?|mins?|m)',
+            value: 1000 * 60
+        },
+        seconds: {
+            label: '(?:seconds?|secs?|s)',
+            value: 1000
+        },
+        milliseconds: {
+            label: '(?:milliseconds?|msecs?|ms)',
             value: 1
         }
     }
